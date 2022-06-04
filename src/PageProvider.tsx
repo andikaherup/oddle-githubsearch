70b073854288bb93755e53582d38dbf9af0d55a4
andika <andikaherup@gmail.com>
2022-06-05 00:28:51 +0700

    first commit

---
+++ src/PageProvider.tsx
@@ -0,0 +1,11 @@
+/* src/PageProvider.tsx */
+import { ThemeProvider } from "@mui/material";
+import { ReactNode } from "react";
+import theme from "./theme";
+interface PageProviderProps {
+  children: ReactNode;
+}
+const PageProvider = ({ children }: PageProviderProps) => {
+  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+};
+export default PageProvider;
