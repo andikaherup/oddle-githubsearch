384896b31fe125a0a0424aeabffa04a3ec026013
andika <andikaherup@gmail.com>
2022-06-07 10:10:16 +0700

    add card component

---
+++ components/UserCards.tsx
@@ -0,0 +1,91 @@
+import * as React from "react";
+// import { IPost } from "../types";
+import Box from "@mui/material/Box";
+import IconButton, { IconButtonProps } from '@mui/material/IconButton';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+type Props = {
+  likeCard: (id: number) => void;
+};
+
+const Post: React.FC<Props> = ({ likeCard }) => {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: { xs: "column", md: "row" },
+        alignItems: "center",
+        bgcolor: "background.paper",
+        overflow: "hidden",
+        borderRadius: "12px",
+        boxShadow: 1,
+        fontWeight: "bold",
+      }}
+    >
+       
+      <Box
+        component="img"
+        sx={{
+          height: 64,
+          width: 64,
+        
+        }}
+        alt="The house from the offer."
+        src="https://images.unsplash.com/photo-1512917774080-9991f1c4c750?auto=format&w=350&dpr=2"
+      />
+      
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: { xs: "center", md: "flex-start" },
+          m: 3,
+          minWidth: { md: 350 },
+        }}
+      >
+        <IconButton aria-label="add to favorites">
+          <FavoriteIcon />
+        </IconButton>
+        <Box component="span" sx={{ fontSize: 16, mt: 1 }}>
+          Bob
+        </Box>
+
+        <Box
+          sx={{
+            mt: 1.5,
+          
+            borderRadius: "5px",
+            fontWeight: "medium",
+            display: "flex",
+            fontSize: 12,
+            alignItems: "center",
+            "& svg": {
+              fontSize: 21,
+              mr: 0.5,
+            },
+          }}
+        >
+          17,3K Followers
+        </Box>
+        <Box
+          sx={{
+         
+          
+            borderRadius: "5px",
+            fontWeight: "medium",
+            display: "flex",
+            fontSize: 12,
+            alignItems: "center",
+            "& svg": {
+              fontSize: 21,
+              mr: 0.5,
+            },
+          }}
+        >
+          103 Following
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default Post;
--- pages/index.tsx
+++ pages/index.tsx
@@ -12,6 +12,7 @@ import BottomNavigationAction from "@mui/material/BottomNavigationAction";
 import RestoreIcon from "@mui/icons-material/Restore";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import TextField from "@mui/material/TextField";
+import UserCards from "../components/UserCards";
 import ArchiveIcon from "@mui/icons-material/Archive";
 const Home: NextPage = () => {
   const { theme, resolvedTheme, setTheme } = useTheme();
@@ -19,7 +20,7 @@ const Home: NextPage = () => {
   const [value, setValue] = useState(0);
   return (
     <Box sx={{ pb: 7, display: "flex", justifyContent: "center" }}>
-      <Box
+       <Box
         width="33vw"
         height="100vh"
         sx={{
@@ -72,7 +73,7 @@ const Home: NextPage = () => {
             alignItems: "center",
           }}
         >
-             <Box
+        <Box
           component="img"
           sx={{ width: "120px" }}
           alt="github logo"
@@ -86,68 +87,11 @@ const Home: NextPage = () => {
             repositories, followers and following.
           </Typography>
         </Box>
-
-        {/* <Box
-      sx={{
-        display: 'flex',
-        flexDirection: { xs: 'column', md: 'row' },
-        alignItems: 'center',
-        bgcolor: 'background.paper',
-        overflow: 'hidden',
-        borderRadius: '12px',
-        boxShadow: 1,
-        fontWeight: 'bold',
-      }}
-    >
-      <Box
-        component="img"
-        sx={{
-          height: 233,
-          width: 350,
-          maxHeight: { xs: 233, md: 167 },
-          maxWidth: { xs: 350, md: 250 },
-        }}
-        alt="The house from the offer."
-        src="https://images.unsplash.com/photo-1512917774080-9991f1c4c750?auto=format&w=350&dpr=2"
-      />
-      <Box
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: { xs: 'center', md: 'flex-start' },
-          m: 3,
-          minWidth: { md: 350 },
-        }}
-      >
-        <Box component="span" sx={{ fontSize: 16, mt: 1 }}>
-          123 Main St, Phoenix AZ
-        </Box>
-        <Box component="span" sx={{ color: 'primary.main', fontSize: 22 }}>
-          $280,000 — $310,000
-        </Box>
-        <Box
-          sx={{
-            mt: 1.5,
-            p: 0.5,
-            backgroundColor: (theme) => alpha(theme.palette.primary.main, 0.1),
-            borderRadius: '5px',
-            color: 'primary.main',
-            fontWeight: 'medium',
-            display: 'flex',
-            fontSize: 12,
-            alignItems: 'center',
-            '& svg': {
-              fontSize: 21,
-              mr: 0.5,
-            },
-          }}
-        >
-          <ErrorOutlineIcon />
-          CONFIDENCE SCORE 85%
-        </Box>
-      </Box>
-      </Box> */}
-      </Box>
+        <UserCards ></UserCards>
+       
+      </Box> 
+      
+    
       <Paper sx={{ width: "33vw", position: "fixed", bottom: 0 }} elevation={3}>
         <BottomNavigation
           sx={{  ...(resolvedTheme === 'light' && {
